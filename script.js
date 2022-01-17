eda4cc8bc5dad7cdc88a9c2930d66d90839e7e59
RutaAuzniece <97448426+RutaAuzniece@users.noreply.github.com>
2022-01-17 15:04:00 +0200

    Sākums

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-alert(1)
+const vardi=['Santa Bērziņa','Pēteris Vanags','Zane Kalna','Anna Sars','Vilnis Sakta',];
+const balvas=['Ledusskapis','Virtuves kombains','Blenderis','Mašīna','Augļu grozs'];
+const naudaKopa=1000000;//kopējā summa izlozei
+let uzvaretajuSkaits=5;
+let rand= Math.random()*10;
+rand= Math.floor(rand);//noapaļo uz leju
+console.log(vardi[rand]);//izvada konsolē nejauši izvēlētus vārdus

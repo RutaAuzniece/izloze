f2763b914fac412deecf111acd64504d0feacb5f
RutaAuzniece <97448426+RutaAuzniece@users.noreply.github.com>
2022-01-24 15:03:56 +0200

    Otrā stunda

--- script.js
+++ script.js
@@ -2,8 +2,15 @@ const vardi=['Santa Bedre','Edvards Vanags','Zane Kalna','Anna Sars','Vilnis Sak
 const balvas=['Ledusskapis','Virtuves kombains','Blenderis','Mašīna','Augļu grozs'];
 const naudaKopa=1000000;//kopējā summa izlozei
 let uzvaretajuSkaits=5;
-for  (let i=0;i<5;i++){
+let rindas= document.querySelector('.rindas');
+rindas.innerHTML=' ';
+for  (let i=0;i<uzvaretajuSkaits;i++){
 let rand= Math.random()*vardi.length;
 rand= Math.floor(rand);//noapaļo uz leju
 console.log(vardi[rand]);//izvada konsolē nejauši izvēlētus vārdus
 }
+rindas.innerHTML=
+<tr>
+    <td>i</td>
+    <td>uzvarētājs</td>
+</tr>
